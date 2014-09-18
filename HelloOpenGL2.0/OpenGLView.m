2400abf4465233f20e901053ded59b86598db35e
mrshyi <mrshyi@sina.com>
2014-09-18 15:42:14 +0800

    Compiling Vertex and Fragment Shaders

--- HelloOpenGL2.0/OpenGLView.m
+++ HelloOpenGL2.0/OpenGLView.m
@@ -74,6 +74,7 @@
         [self setupContext];
         [self setupRenderBuffer];
         [self setupFrameBuffer];
+        [self compileShaders];
         [self render];
     }
     return self;
@@ -84,9 +85,74 @@
     _context = nil;
 }
 
+/**********************************/
+-(GLuint)compileShader:(NSString*)shaderName withType:(GLenum)shaderType{
+    
+    // 1
+    NSString *shaderPath = [[NSBundle mainBundle] pathForResource:shaderName ofType:@"glsl"];
+    NSError *error;
+    NSString *shaderString = [NSString stringWithContentsOfFile:shaderPath encoding:NSUTF8StringEncoding error:&error];
+    if (!shaderString) {
+        NSLog(@"Error loading shader:%@",error.localizedDescription);
+        exit(1);
+    }
 
+    // 2 create a opengl object to represent the shader
+    GLuint shaderHandle = glCreateShader(shaderType);
+    
+    // 3
+    const char *shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = (int)[shaderString length];
+    glShaderSource(shaderHandle, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    // 4
+    glCompileShader(shaderHandle);
+    
+    // 5
+    GLint compileSuccess;
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shaderHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@",messageString);
+        exit(1);
+    }
+    return shaderHandle;
+}
 
-
+-(void)compileShaders{
+    
+    // 1
+    GLuint vertexShader = [self compileShader:@"SimpleVertex" withType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:@"SimpleFragment" withType:GL_FRAGMENT_SHADER];
+    
+    // 2
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    glLinkProgram(programHandle);
+    
+    // 3
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@",messageString);
+        exit(1);
+    }
+    
+    // 4
+    glUseProgram(programHandle);
+    
+    // 5
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _colorSlot = glGetAttribLocation(programHandle, "SourceColor");
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_colorSlot);
+}
 
 
 
