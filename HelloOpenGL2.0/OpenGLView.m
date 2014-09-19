efa021475086053878939aa7bb945af54ab7915f
mrshyi <mrshyi@sina.com>
2014-09-19 14:11:46 +0800

    Gratuitous 3D Cube

--- HelloOpenGL2.0/OpenGLView.m
+++ HelloOpenGL2.0/OpenGLView.m
@@ -8,6 +8,8 @@
 
 #import "OpenGLView.h"
 #import "CC3GLMatrix.h"
+// include generated arrays
+#import "./asm0005.h"
 
 @implementation OpenGLView
 
@@ -201,6 +203,7 @@ typedef struct {
     float Color[4];
 } Vertex;
 
+/*
 const Vertex Vertices[] = {
     {{1, -1, 0}, {1, 0, 0, 1}},
     {{1, 1, 0}, {0, 1, 0, 1}},
@@ -211,6 +214,38 @@ const Vertex Vertices[] = {
 const GLubyte Indices[] = {
     0, 1, 2,
     2, 3, 0
+};
+*/
+const Vertex Vertices[] = {
+    {{1, -1, 0}, {1, 0, 0, 1}},
+    {{1, 1, 0}, {1, 0, 0, 1}},
+    {{-1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, -1, 0}, {0, 1, 0, 1}},
+    {{1, -1, -1}, {1, 0, 0, 1}},
+    {{1, 1, -1}, {1, 0, 0, 1}},
+    {{-1, 1, -1}, {0, 1, 0, 1}},
+    {{-1, -1, -1}, {0, 1, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    // Front
+    0, 1, 2,
+    2, 3, 0,
+    // Back
+    4, 6, 5,
+    4, 7, 6,
+    // Left
+    2, 7, 3,
+    7, 6, 2,
+    // Right
+    0, 4, 1,
+    4, 1, 5,
+    // Top
+    6, 2, 1,
+    1, 6, 5,
+    // Bottom
+    0, 3, 7,
+    0, 7, 4
 };
 
 - (void)setupVBOs {
